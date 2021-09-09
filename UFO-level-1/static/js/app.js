39c9b319aed6aa160ce501b8330bc82ced7cdc8d
speedracer05 <speedracer-05@outlook.com>
2021-09-09 10:59:49 -0700

    index.html add table border table-sm totable id=ufo-table. style.css add sort code, app.js add search function and enable-disable function.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,21 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// To manipulate tble search 
+$(document).ready(function () {
+  $('#ufo-table').DataTable();
+  $('.dataTables_length').addClass('bs-select');
+  });
+
+// Enable / disable search. Enabled by default
+$(document).ready(function () {
+  $('#ufo-table').DataTable({
+  "searching": false // false to disable search (or any other option)
+  });
+  $('.dataTables_length').addClass('bs-select');
+  });
+
+console.log(data)
