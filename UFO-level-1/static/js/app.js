d86d05ed08cbe4bf5943d5b241f27deb0e60209d
speedracer05 <speedracer-05@outlook.com>
2021-09-09 14:14:45 -0700

    completed forEach loop and data retrieval

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,15 +1,16 @@
 // from data.js
-var tableData = data;
+var dataSighting = data;
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-
-// Loop through data and log observation object and append one tble row
-var addData = (dataInput) => {
+// Loop through data and log observation object and append one cell for each value
   dataSighting.forEach(sightings => {
-      var row = $tbody.append("tr");
-      columns.forEach(column => row.append("td").text(sightings[column])
-      )
+    var row = tbody.append("tr");
+    Object.entries(sightings).forEach(([key, value]) => {
+      var cell = row.append("td");
+      console.log(sightings)
+
+      cell.text(value);
   });
-}
+});
